353151e719f6d8334d5e5565f38aa91b1145a639
Sagar Gupta <128049788+Sagargupta028@users.noreply.github.com>
2025-06-23 19:58:57 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const mondbUrl = process.env.MONGODB_URL || "mongodb+srv://sagargupta028:Sagar@9097@cluster0.tpyfy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mondbUrl = process.env.MONGODB_URL
 
 const connectDb = () =>{
     console.log('connecting to server')
